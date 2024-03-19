51d4485f507de9c65aae0750dde6a47dcbdad053
Eugene <sifmeop@gmail.com>
2024-03-19 12:53:34 +0200

    ff

--- src/App.tsx
+++ src/App.tsx
@@ -69,7 +69,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h2>{token}</h2>
-      <h2>{Notification.permission}</h2>
+      <h2>{Notification?.permission}</h2>
       <div className='card'>
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
